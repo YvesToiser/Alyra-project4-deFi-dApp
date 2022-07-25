0108fbc96c7003a188113cc3d2d92df3cd728a4b
xavier <xav888@msn.com>
2022-07-25 21:30:50 +0200

    ajout EthUsdPrice operationnel

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -1,12 +1,18 @@
 // 2_deploy_contracts.js
 const Byx = artifacts.require("Byx");
 const ByxManager = artifacts.require("ByxManager");
+const EthUsdPrice = artifacts.require("EthUsdPrice");
 
 module.exports = async function(deployer, _network, accounts) {
-    await deployer.deploy(Byx);
+    await deployer.deploy(EthUsdPrice)
+	const ethUsdPrice = await EthUsdPrice.deployed();
+	
+	await deployer.deploy(Byx);
     const byx = await Byx.deployed();
+
 	await deployer.deploy(ByxManager, byx.address);
 	const byxManager = await ByxManager.deployed();
+
 	await byx.faucet(ByxManager.address, 100);
 	await byxManager.byxTransfer(accounts[1], 100);
  
