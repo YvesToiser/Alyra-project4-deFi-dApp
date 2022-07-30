78ae1efd30f0db3182cf64916b29c4e4d743d532
Ben-Rey <44082144+Ben-Rey@users.noreply.github.com>
2022-07-30 21:20:26 +0200

    SETUP - Add chakra ui

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { EthProvider } from "contexts/EthContext";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac"
+  }
+};
+
+const theme = extendTheme({ colors });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <EthProvider>
-      <App />
-    </EthProvider>
+    <ChakraProvider theme={theme}>
+      <EthProvider>
+        <App />
+      </EthProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
