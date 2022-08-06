e947fe445d674bd4b6b3ccb62a43380bd76a3819
Ben-Rey <44082144+Ben-Rey@users.noreply.github.com>
2022-08-07 00:25:25 +0200

    Feat - get tvl from events

--- client/src/api/tokenManagerEth.js
+++ client/src/api/tokenManagerEth.js
@@ -1,9 +1,35 @@
+import { Big } from "big.js";
+
 export async function getEthApr(contract) {
   return await contract.methods.APR().call();
 }
 
 export async function getEthTVL(contract) {
-  return await contract.methods.getTotalStaked().call();
+  const stakeLogs = await contract.getPastEvents("Stake", {
+    // user
+    // filter: { user: address },
+    fromBlock: 0,
+    toBlock: "latest"
+  });
+
+  const withdrawLogs = await contract.getPastEvents("WithdrawStake", {
+    // user
+    // filter: { user: address },
+    fromBlock: 0,
+    toBlock: "latest"
+  });
+
+  const totalStake = stakeLogs.reduce((acc, curr) => {
+    return acc.plus(curr.returnValues.amount);
+  }, new Big(0));
+
+  const totalWithdraw = withdrawLogs.reduce((acc, curr) => {
+    return acc.plus(curr.returnValues.amount);
+  }, new Big(0));
+
+  const tvl = totalStake.minus(totalWithdraw);
+
+  return tvl;
 }
 
 export function getEthUserInfo(contract, address) {
--- client/src/api/web3.js
+++ client/src/api/web3.js
@@ -6,12 +6,6 @@ export async function getChainId(web3) {
 export async function getNetworkId(web3) {
   return await web3.eth.net.getId();
 }
-function roundBalance(_balance) {
-  const num = Number(_balance);
-  const roundedString = num.toFixed(4);
-  const rounded = Number(roundedString);
-  return rounded;
-}
 
 export async function getBalance(web3, address) {
   if (web3 && address) {
